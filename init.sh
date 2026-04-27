aee9250caf5e8ca41eea562e357d5b4363173a8f
linhsuanyu <wayne315315@gmail.com>
2026-04-27 17:38:54 +0800

    Modify requirements.txt & initial script for cluster

--- init.sh
+++ init.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-pip install cython==3.2.4
+
+# git clone
+git clone https://github.com/wayne315315/blokus.git
+git checkout torch
+cd blokus
+
+# Install dependencies
+apt update && apt install -y python3.12-dev python3.12-venv
+python3.12 -m venv .venv
+printf '\n# Manually set CUDA 13.0 paths for Verda B300\nexport CUDA_HOME=/usr/local/cuda-13.0\nexport LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH\n' >> .venv/bin/activate
+source .venv/bin/activate
+
+# install pip packages
+pip install -r requirements.txt
 python setup.py build_ext --inplace
